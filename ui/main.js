dcad98e380292bfbc9ee39243af90ffee7be11af
neerajvijh <vijh.gr8@gmail.com>
2016-10-06 17:11:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,4 @@
-console.log('Loaded!');
-
-// change the text
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
-
-//Move the image
-var img = document.getElementById('nee');
-var marginLeft = 0;
-
-function moveRight(){
-            marginLeft = marginLeft + 1;
-            img.style.marginLeft = marginLeft + 'px';
-            
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-
-    
-} ;
+//counter code
 var counter = 0;
 var button = document.getElementById('counter');
 button.onclick = function(){
