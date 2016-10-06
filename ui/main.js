39069787bd5de284a027cdd745073266eb184d43
neerajvijh <vijh.gr8@gmail.com>
2016-10-06 20:13:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,7 @@ button.onclick = function(){
       if (request.getReadyState === XMLHttpRequest.DONE) {
       //Take some action
       if (request.status === 200 ) {
-         var counter = request.responseText;
-         var span = document.getElementById('count');
-         span.innerHTML = counter.toString();
+        
          
       }
       
@@ -32,19 +30,24 @@ button.onclick = function(){
         
         var submit = document.getElementById('submit_btn');
         submit.onclick = function(){
-            //Make a request to server & send the name
-            
+  //Create a requst object
+  var request = new XMLHttpRequest();
             //capture a list of names & render it as a list
-            var names = request.responseText;
-            names = JSON.pars(names);
-            var list = '';
-            for ( var i = 0; i < names.length; i++){
-                list += '<li>' + names[i] + '</li>';
-            }
-            
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
-   
+request.onreadystatechange = function (){
+      if (request.getReadyState === XMLHttpRequest.DONE) {
+      //Take some action
+      if (request.status === 200 ) {
+        var names = JSON.parse(names);
+        
+        var list='';
+        for(var i =0; i<names.length; i++){
+            list +='<li>' + names[i] + '</li>';
+        }
+         var ul = document.getElementById('namelist');
+         ul.innerHTML = list;
+      }
+      }
+   };
        //Make the request
     var nameInput = document.getElementById('name');
         var name1 = nameInput.value;   
