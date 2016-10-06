a81bb6d7c75f752c9b44e5be166ba5a758848a8c
neerajvijh <vijh.gr8@gmail.com>
2016-10-06 19:18:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,5 +26,22 @@ button.onclick = function(){
  //Make the request
  request.open('GET','http://neerajvijh.imad.hasura-app.io/counter', true);
  request.send(null);
-
 };
+
+//Submit name
+        var nameInput = document.getElementById('name');
+        var name1 = nameInput.value;
+        var submit = document.getElementById('submit_btn');
+        submit.onclick = function(){
+            //Make a request to server & send the name
+            
+            //capture a list of names & render it as a list
+            var names = ['name1', 'name2', 'name3'];
+            var list = '';
+            for ( var i = 0; i < names.length; i++){
+                list += '<li>' + names[i] + '</li>';
+            }
+            
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+        };
