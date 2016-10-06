64b1603819db27701b2f9f9cb2c18ff96921cb53
neerajvijh <vijh.gr8@gmail.com>
2016-10-06 11:02:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ var articleOne = {
            `
 }
 function createTemplate(data) {
+            var title = data.title;
+            var date = data.date;
+            var heading = data.heading;
+            var content = data.content;
             var htmlTemplate = `
             <html>
                <head>
