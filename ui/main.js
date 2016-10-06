7551e542752ab5f0f52a7ca91da8ab46b3fce0f4
neerajvijh <vijh.gr8@gmail.com>
2016-10-06 16:23:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ element.innerHTML = "New Value";
 
 //Move the image
 var img = document.getElementById('nee');
-img.onClick = function(){
+img.onclick = function(){
     img.style.maginLeft = '100px';
 } ;
