a7a3587b78f05472e50e9c779a46455f6796fbfb
neerajvijh <vijh.gr8@gmail.com>
2016-10-24 17:07:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var config = {
   user: 'neerajvijh',
   database: 'neerajvijh',
-  host: 'http://db.imad.hasura-app.io/',
+  host: 'db.imad.hasura-app.io',
   port : '5432',
   password: process.env.DB_PASSWORD
 };
