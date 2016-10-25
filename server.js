340be6f6b0781ebc7e8902f96d002f5b2cb34aaf
neerajvijh <vijh.gr8@gmail.com>
2016-10-25 11:52:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool
 var app = express();
+var Pool = require('pg').Pool;
 
 var config = {
   user: 'neerajvijh',
