edd87fb36dad2515c11d11a6f04eca93eb08ee51
neerajvijh <vijh.gr8@gmail.com>
2016-10-25 12:32:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('articles/:articleName', function (req, res) {
               var articleData = result.rows[0];
               res.send(createTemplate(articles[articleData]));
           }
-          res.send(JSON.stringify(result.rows));
+         
       }
     });
 });
