7af594c8d5553a4e5323a46d7d7727e3abd4d257
neerajvijh <vijh.gr8@gmail.com>
2016-10-26 09:27:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,6 +124,27 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
+app.get('/articles/:articleName', function (req, res) {
+    //articleName = article-one
+   
+    pool.query("SELECT * FROM test_article WHERE title =" + req.params.articleName, function(err, result) {
+    if (err) 
+      {
+          res.status(500).send(err.toString());
+      }
+      else{
+          if(RESULT.ROWS.LENGTH === 0) {
+                res.status(404).send('Article not found');
+          }
+          else{
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+          
+      }
+   });
+  
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
